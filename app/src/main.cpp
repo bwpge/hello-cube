9a6a31b5e6e3a3c38177bb6027082e2b2a31833a
bwpge <bwpge.dev@gmail.com>
2023-08-24 19:12:31 -0400

    Use heap allocation for large engine object

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,12 +1,14 @@
+#include <memory>
+
 #include "hello_cube.hpp"
 
 i32 main() {
-    hc::Engine engine{"Hello Triangle", 1600, 900};
+    auto engine = std::make_unique<hc::Engine>("Hello Cube", 1920, 1080);
 
     try {
-        engine.init();
-        engine.run();
-        engine.cleanup();
+        engine->init();
+        engine->run();
+        engine->cleanup();
     } catch (const std::exception& e) {
         spdlog::critical("UNHANDLED EXCEPTION: {}", e.what());
     } catch (...) {
