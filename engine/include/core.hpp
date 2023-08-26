596629838c787de6c7141cd36e889c45d581d66d
bwpge <bwpge.dev@gmail.com>
2023-08-26 14:15:22 -0400

    Add runtime assertion macro

--- engine/include/core.hpp
+++ engine/include/core.hpp
@@ -4,8 +4,19 @@
 
 #include "types.hpp"
 
+#define _STRINGIFY(arg) #arg
+#define STRINGIFY(arg) _STRINGIFY(arg)
+
 #define PANIC(msg)                                                      \
     do {                                                                \
         spdlog::critical("PANIC: {} ({}:{})", msg, __FILE__, __LINE__); \
         throw std::runtime_error(msg);                                  \
+    } while (0)
+
+#define HC_ASSERT(expr, msg)                                         \
+    do {                                                             \
+        if (!(expr)) {                                               \
+            spdlog::error("FAILED ASSERTION `{}`", STRINGIFY(expr)); \
+            PANIC(msg);                                              \
+        }                                                            \
     } while (0)
